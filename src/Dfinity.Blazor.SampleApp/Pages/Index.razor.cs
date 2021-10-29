b6062aa2e6049b073637dafa15a5491036812c89
michielpost <michiel@q42.nl>
2021-10-29 10:24:29 +0200

    Added output window

--- src/Dfinity.Blazor.SampleApp/Pages/Index.razor.cs
+++ src/Dfinity.Blazor.SampleApp/Pages/Index.razor.cs
@@ -6,24 +6,34 @@ using System.Threading.Tasks;
 
 namespace Dfinity.Blazor.SampleApp.Pages
 {
-    public partial class Index : IDisposable
+    public partial class Index
     {
         [Inject]
         public DfinityService DfinityService { get; set; } = default!;
 
+        public string Output { get; set; } = "Output:";
+
         public async Task Test()
         {
             await DfinityService.Test();
+            WriteOutput($"Test");
+
         }
 
         public async Task WriteData()
         {
-            await DfinityService.SetValue("time", DateTimeOffset.UtcNow.ToString());
+            string value = DateTimeOffset.UtcNow.ToString();
+            await DfinityService.SetValue("time", value);
+            WriteOutput($"Written value: {value}");
         }
+
+        
         public async Task GetData()
         {
-            string value = await DfinityService.GetValue("time");
+            string? value = await DfinityService.GetValue("time");
             Console.WriteLine("Value: " + value);
+            WriteOutput($"Get value: {value}");
+
         }
 
 
@@ -31,33 +41,46 @@ namespace Dfinity.Blazor.SampleApp.Pages
         {
             bool isLoggedIn = await DfinityService.IsLoggedIn();
             Console.WriteLine("Logged in: " + isLoggedIn);
+            WriteOutput($"Logged in: {isLoggedIn}");
+
         }
 
         public async Task Login()
         {
             await DfinityService.Login();
+            WriteOutput($"Opend logged in window");
+
         }
 
         public async Task Logout()
         {
             await DfinityService.Logout();
+            WriteOutput($"Loggout");
+
         }
 
 
         public async Task WriteDataForUser()
         {
-            await DfinityService.SetValueForUser("time", DateTimeOffset.UtcNow.ToString());
+            string value = DateTimeOffset.UtcNow.ToString();
+            await DfinityService.SetValueForUser("time", value);
+            WriteOutput($"Write value for current user: {value}");
+
         }
         public async Task GetDataForUser()
         {
             var value = await DfinityService.GetValueForUser("time");
             Console.WriteLine("Uservalue: " + value);
+            WriteOutput($"Get value for current user: {value}");
+
 
         }
 
 
-        public void Dispose()
+        private void WriteOutput(string text)
         {
+            Output += "\r\n" + text;
         }
+
     }
 }
